837c957f0b1a9cb10847767a2f65e18b00def2e5
Fereydoon Karimi <freydoonk@gmail.com>
2023-08-20 17:41:23 +0100

    Update constants

--- src/library/constants.ts
+++ src/library/constants.ts
@@ -1,2 +1,5 @@
 export const PUBLIC_URL = process.env.PUBLIC_URL || "/";
 export const API_URL = process.env.API_URL as string;
+export const NUMBERS_COUNT = 7;
+export const MACHINE_INDEX = 11;
+export const NUM_OF_SUGGESTIONS = 6;
